4249dac986b3c2ae07ffb7d758f1dcbf932c7bff
RATHI <102493321+CharithPramodyaSenanayake@users.noreply.github.com>
2023-04-23 06:38:39 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
+ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'I Am Alive Now' : process.env.ALIVE_MSG,
 LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
-ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://te.legra.ph/file/7cc522af6b9c0aeb8b62d.jpg' : process.env.ALIVE_LOGO,
+ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i0.wp.com/trenovision.com/wp-content/uploads/2020/03/official-WHO-bot.jpg' : process.env.ALIVE_LOGO,
 FOOTER: process.env.FOOTER === undefined ? 'ᴘᴇᴀᴄᴇ ᴍᴅ' : process.env.FOOTER,
 CAPTION: process.env.CAPTION === undefined ? 'ᴘᴇᴀᴄᴇ ᴍᴅ ʙʏ ᴄʜᴀʀɪᴛʜ' : process.env.CAPTION,
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
